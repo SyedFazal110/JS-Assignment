c3ac9036e895a18276e02263b757b6f302a75ed4
SyedFazal110 <syedfazal808@gmail.com>
2024-02-29 21:24:19 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,13 @@
+// Question NO:2
+alert("Error! Please enter a valid Password.")
+
+// Question NO:3
+alert("Welcome to JS Land... \n Happy Coding!")
+
+// Question NO:4
+alert("Welcome to JS Land...")
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+// Question NO:5
+alert("Hello... I can run JS through my web browser's console")
+

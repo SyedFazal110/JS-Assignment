a00a85d55f8b42a515eda08a738a88166966b4c0
SyedFazal110 <syedfazal808@gmail.com>
2024-03-02 22:23:26 +0500

    index file created

---
+++ app2.js
@@ -0,0 +1,36 @@
+// // Question No:1
+// var userName = "USERNAME"
+
+// // Question No:2
+// var myName = "Syed Fazal ALi Jafferi"
+
+// Question No:3
+// var message = "Hello, World!"
+// alert(message);
+
+// Question No:4
+// var name = "Fazal Ali"
+// var age = "23"
+// var academy = "SMIT"
+// alert(name);
+// alert(age);
+// alert(academy);
+
+// Question No:5
+// var food = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(food);
+
+
+// Question No:6
+// var email = "syedfazal808@gmail.com"
+// alert("My email address is" +" "+ email);
+
+// Question No:7
+// var book = "A smarter \nway to learn JavaScript"
+// alert("I am trying to learn from the Book" +" "+ book);
+
+// Question No:8
+// document.write("Yah! I can write HTML,content through JavaScript");
+
+// Question No:9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");

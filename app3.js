d058799b9e47593546902de05b4ccd28bda523d0
SyedFazal110 <syedfazal808@gmail.com>
2024-03-16 05:17:43 +0500

    js file created

---
+++ app3.js
@@ -0,0 +1,21 @@
+// Question No:1
+// var age = "I am 23 years old"
+// alert(age);
+
+// Question No:2
+// let user = prompt("How many times you visit this Website");
+// alert(user);
+
+
+// Question No:3
+// document.write( "My birth year is 2001 </br>  Data type of my declared variable is Number" );
+
+
+// Question No:4
+// let userName = prompt("Visitor's Name");
+// let quantity = prompt("How msny products a visitor wants to order");
+// let product = prompt("Product Title");
+// document.write(userName ," ", "ordered" ," ", quantity ,"-", product ," ", "on XYZ Clothing store");
+
+
+// Question No:5

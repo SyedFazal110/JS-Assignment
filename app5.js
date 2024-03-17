c3945e5167f844cb914d5d16d72ad1cfe142e893
SyedFazal110 <syedfazal808@gmail.com>
2024-03-17 23:36:48 +0500

    js file created

---
+++ app5.js
@@ -0,0 +1,12 @@
+// Question No:01 & 02
+// let a = 5;
+// let b = 6;
+// console.log(a + b);
+// document.write("sum of 5 and 6 =" , a + b);
+// document.write("</br>sum of 5 and 6 =" , a - b);
+// document.write("</br>sum of 5 and 6 =" , a * b);
+// document.write("</br>sum of 5 and 6 =" , a / b);
+
+
+
+// Question No:03

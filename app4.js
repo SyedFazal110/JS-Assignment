69bccbc91585185327ddf40728c3e571d653e724
SyedFazal110 <syedfazal808@gmail.com>
2024-03-17 18:16:25 +0500

    js file created

--- app4.js
+++ app4.js
@@ -1,1 +1,16 @@
 // Question No:1
+// let1 , let2 , let3 = 8 , "Ball" , "Pool";
+// let name = "Fazal";
+// let $age = 21;
+// let institute = "SMIT";
+// let study_institute = "Mobile and Web App Development";
+// let universyity1address = "FUUAST";
+// let my age = 21;
+// let my-Name = "Fazal";
+// let my-academy1 = "SMIT";
+// let my@course = "WMA";
+// let @uni = "FUUAST";
+document.write("Variable"," ","names"," ","can"," ","only"," ","contain"," ","english,"," ","numbers,"," ","$"," ","and"," ","_",".", "  ", "For Example"," ",":"," ","$my_1stVariable");
+document.write("</br>","Variable"," ","must"," ","begin"," ","with"," ","a"," ","letter,"," ","$"," ","or"," ","_",".", "  ", "For Example"," ",":"," ","$name,_name or name");
+document.write("</br> Variable names are case sensitive");
+document.write("</br> Variable names should not be JS keywords")

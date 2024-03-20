b4784b924dc505f031c501e8af788d737b6a9e4e
SyedFazal110 <syedfazal808@gmail.com>
2024-03-20 23:02:22 +0500

    js file created

--- app5.js
+++ app5.js
@@ -10,3 +10,34 @@
 
 
 // Question No:03
+// let a = 5;
+// a++;
+// document.write("Value after variable declaration is: 5");
+// document.write("</br>initial value: 5");
+// document.write("</br>Vaue after increment is: ",  a);
+// a = a + 7;
+// document.write("</br>Vaue after addition is: ", a);
+// document.write("</br>Vaue after decrement is: ",  --a);
+// a = a / 3;
+// document.write("</br>The remainder is: ", a);
+
+
+
+// Question No:04
+// let ticket1 = 500;
+// let tickets = ticket1 * 5;
+// document.write("Total cost to buy five tickets to a movie is ", tickets ,"PKR");
+
+
+
+// Question No:05
+// let userInput = +prompt("Enter a number");
+// let times = +prompt("How many times");
+// for(let i = 1; i <= 10; i++)
+// {
+//     document.write(`${i} * ${userInput}= ${userInput*i} </br>`)
+// }
+
+
+// Question No:06
+let celsius = 
